ce89ad040b1a9b18453045226d74260b9a95c2d5
DaniaAbughoush <ms1dodo@gmail.com>
2021-05-11 02:20:05 +0300

    final

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -2,21 +2,14 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card'
 // import CardColumns from 'react-bootstrap/CardColumns';
+import MovieDay from '../component/movieDay';
 
 class Movie extends React.Component {
     render() {
         return (
             this.props.movieInfo.map((data, index) => {
                 return (<div key={index}>
-                    <Card style={{ width: '18rem' }}>
-  <Card.Img style={{ width: '10rem' }} variant="top" src={`https://image.tmdb.org/t/p/original/${data.poster}`} />
-  <Card.Body>
-    <Card.Title>{data.title}</Card.Title>
-    <Card.Text>
-    <p>{data.overview} <br/>{data.vote_count}<br></br>{data.release_date}</p>
-    </Card.Text>   
-  </Card.Body>
-</Card>
+  <MovieDay title={data.title} poster={data.poster} overview={data.overview} vote_count={data.vote_count} release_date={data.release_date} />
                   {/* <p>{data.title}</p> */}
                   {/* <p>{data.poster}</p> */}
                   {/* <p>{data.overview}</p> */}
--- src/component/movieDay.js
+++ src/component/movieDay.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-class movieDay extends React.Component {
+class MovieDay extends React.Component {
     render() {
         return (
             <div>
-
+                  <Card style={{ width: '18rem' }}>
+  <Card.Img style={{ width: '10rem' }} variant="top" src={`https://image.tmdb.org/t/p/original/${this.props.poster}`} />
+  <Card.Body>
+    <Card.Title>{this.props.title}</Card.Title>
+    <Card.Text>
+    <p>{this.props.overview} <br/>{this.props.vote_count}<br></br>{this.props.release_date}</p>
+    </Card.Text>   
+  </Card.Body>
+</Card>
      
             </div>
                 
@@ -14,4 +22,4 @@ class movieDay extends React.Component {
         }
     }
 
-    export default movieDay
+    export default MovieDay
